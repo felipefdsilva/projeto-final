64a8eb36c983ccf55ee7ac114ff50148b55dd4d2
Felipe Ferreira <felipeferreira@poli.ufrj.br>
2020-04-25 18:52:12 -0300

    adds field class

--- message.h
+++ message.h
@@ -1,13 +1,22 @@
 #ifndef _MESSAGE_H_
 #define _MESSAGE_H_ "message.h"
 
-typedef struct {
-    String id;
-    String type;
-    String lat;
-    String lng;
-} Message;
-
-Message processMessage(char *, unsigned);
-
-#endif
+class Field{
+    public:
+        Field();
+        int getFieldSize();
+        int getFieldValue();
+        uint16_t insert(uint16_t, uint16_t, uint16_t);
+        uint16_t extract(uint16_t, uint16_t, uint16_t);
+    private:
+        unsigned short size;
+        unsigned short value;
+};
+class Message{
+    public:
+        Message();
+        Field getField(string);
+    private:
+        uint16_t *message;
+        unsigned short size;
+};
